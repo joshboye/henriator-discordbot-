4e6e1d1761a4957de53a1236c761a85bb08b6bf7
joshboye <joshboye8@gmail.com>
2022-07-25 04:21:54 +0530

    stuffchanged

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js')
 require("dotenv").config()
 
-// const TOKEN = "MTAwMDgyMDM1MTQ3Njk1NzE4Nw.G54X3-.ZGHXwspkQRA5WSPss0hB73DrYH7CjhVbR2F1NE"
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] })
 
 client.login(process.env.TOKEN)
